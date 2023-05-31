d483a4e7f7189c9a36ea8607da08e827314a97fc
Edduard79 <eduard.22@gmail.com>
2023-06-01 01:06:00 +0200

    Hope it works!

---
+++ src/main/java/com/example/Ex25/MessageController.java
@@ -0,0 +1,20 @@
+package com.example.Ex25;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class MessageController {
+
+    @Autowired
+    SimpMessagingTemplate simpMessagingTemplate;
+
+    @PostMapping("/message")
+    public void sendNotification(@RequestBody MessageDTO messageDTO) {
+        simpMessagingTemplate.convertAndSend("/topic/broadcast-message", messageDTO);
+    }
+}
---
+++ src/main/java/com/example/Ex25/MessageDTO.java
@@ -0,0 +1,30 @@
+package com.example.Ex25;
+
+public class MessageDTO {
+    private String type;
+    private String message;
+
+    public MessageDTO() {
+    }
+
+    public MessageDTO(String type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
